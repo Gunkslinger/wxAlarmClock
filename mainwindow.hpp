bfe0c6826d2f10687f4e4bc2241e8fc7bddba814
fcf <fcf@fastmail.com>
2025-11-13 10:52:51 -0500

    switch to AM/PM toggle button

--- mainwindow.hpp
+++ mainwindow.hpp
@@ -1,6 +1,6 @@
 #include <wx/wx.h>
 #include <wx/spinctrl.h>
-#include <wx/checkbox.h>
+#include <wx/tglbtn.h>
 #include <wx/timer.h>
 #include <wx/event.h>
 
@@ -10,13 +10,14 @@ public:
     AlarmControlFrame();
     wxSpinCtrl *spinHour;
     wxSpinCtrl *spinMinute;
-    wxCheckBox *checkBoxAMPM;
+    wxToggleButton *toggleButtonAMPM;
     wxButton *buttonStartStop;
     wxStaticText* labelAlarmTime;
     wxBoxSizer *spinSizer;
     wxBoxSizer *mainsizer;
     wxTimer *timer;
     void OnExit(wxCommandEvent& event);
+    void OnToggle(wxCommandEvent& event);
     void StartStop(wxCommandEvent& event);
     void CountDown(wxTimerEvent& event);
 };
